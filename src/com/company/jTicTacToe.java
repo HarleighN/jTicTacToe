42e705a29484a51681befb0a0898eeb92157c59d
Harleigh Niyu <hillf@duq.edu>
2022-06-11 22:06:05 -0400

    Menu added, Random AI player added

--- src/com/company/jTicTacToe.java
+++ src/com/company/jTicTacToe.java
@@ -7,16 +7,34 @@ import java.util.*;
 
 public class jTicTacToe {
     public static void main(String[] args) {
-        Game board = new Game();
         Random r = new Random();
+        Game board = new Game();
+        Player PlayerX = new Player('x');
+        Player PlayerO = new Player('o');
+        Scanner kb = new Scanner(System.in);
 
-        Player Player1 = new Player( 'x');
-        Player Player2 = new Player('o');
+        System.out.println("Game Options:");
+        System.out.println("(1)\t\t Human v. Human");
+        System.out.println("(2)\t\t Human v. AI");
+        System.out.println("(3)\t\t AI v. AI");
+        System.out.println("(Any)\t Quit");
 
-        board.print();
+        switch (kb.nextInt()) {
+            case 1 -> twoHumanPlayers(board, PlayerX, PlayerO);
+            case 2 -> {
+                if(r.nextInt(1,3) == 1) aiFirst(board, PlayerX, PlayerO);
+                else aiSecond(board, PlayerX, PlayerO);
+            }
+            case 3 -> twoAIPlayers(board, PlayerX, PlayerO);
+            default -> System.out.println("Game Over."); //idk this is in case they start the game but decide not to play lol
+        }
+    }
 
+    public static void twoHumanPlayers(Game board, Player Player1, Player Player2) {
         int counter = 0;
         Scanner sc = new Scanner(System.in);
+
+        board.print();
         while (!(board.end() == 'x' || board.end() == 'o') && counter < 9) {
 
             System.out.println("Player 1's Turn: ");
@@ -26,7 +44,65 @@ public class jTicTacToe {
             board.print();
             counter++;
 
-            if(board.end() == 'x' || board.end() == 'o' || counter == 9) break; //Ends the game if Player1 already won or a draw has happened
+            if (board.end() == 'x' || board.end() == 'o' || counter == 9)
+                break; //Ends the game if Player1 already won or a draw has happened
+
+            System.out.println("Player 2's Turn: ");
+            while (!(Player2.place(board, sc.nextInt()))) { //!place(board.board, Player2, sc.nextInt())
+                System.out.println("There's already a piece there. Please try again.");
+            }
+            board.print();
+            counter++;
+        }
+
+        endMessage(board);
+    }
+
+    public static void aiFirst(Game board, Player Player1, Player Player2) {
+        int counter = 0;
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        board.print();
+        while (!(board.end() == 'x' || board.end() == 'o') && counter < 9) {
+
+            System.out.println("Player 1's Turn: ");
+            while (!(Player1.place(board, r.nextInt(1,10)))) { //!place(board.board, Player1, sc.nextInt())
+                System.out.println("There's already a piece there. Please try again.");
+            }
+            board.print();
+            counter++;
+
+            if (board.end() == 'x' || board.end() == 'o' || counter == 9)
+                break; //Ends the game if Player1 already won or a draw has happened
+
+            System.out.println("Player 2's Turn: ");
+            while (!(Player2.place(board, sc.nextInt()))) { //!place(board.board, Player2, sc.nextInt())
+                System.out.println("There's already a piece there. Please try again.");
+            }
+            board.print();
+            counter++;
+        }
+        endMessage(board);
+    }
+
+    public static void aiSecond(Game board, Player Player1, Player Player2) {
+        int counter = 0;
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        board.print();
+        while (!(board.end() == 'x' || board.end() == 'o') && counter < 9) {
+
+            System.out.println("Player 1's Turn: ");
+            while (!(Player1.place(board, sc.nextInt()))) { //!place(board.board, Player1, sc.nextInt())
+                System.out.println("There's already a piece there. Please try again.");
+            }
+            board.print();
+            counter++;
+
+            if (board.end() == 'x' || board.end() == 'o' || counter == 9)
+                break; //Ends the game if Player1 already won or a draw has happened
 
             System.out.println("Player 2's Turn: ");
             while (!(Player2.place(board, r.nextInt(1, 10)))) { //!place(board.board, Player2, sc.nextInt())
@@ -35,9 +111,40 @@ public class jTicTacToe {
             board.print();
             counter++;
         }
+        endMessage(board);
+    }
 
-        if(board.end() == 'x') System.out.println("Player 1 wins!");
-        else if(board.end() == 'o') System.out.println("Player 2 wins!");
+    public static void twoAIPlayers(Game board, Player Player1, Player Player2) {
+        int counter = 0;
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        board.print();
+        while (!(board.end() == 'x' || board.end() == 'o') && counter < 9) {
+
+            System.out.println("Player 1's Turn: ");
+            while (!(Player1.place(board, r.nextInt(1, 10)))) { //!place(board.board, Player1, sc.nextInt())
+                System.out.println("There's already a piece there. Please try again.");
+            }
+            board.print();
+            counter++;
+
+            if (board.end() == 'x' || board.end() == 'o' || counter == 9)
+                break; //Ends the game if Player1 already won or a draw has happened
+
+            System.out.println("Player 2's Turn: ");
+            while (!(Player2.place(board, r.nextInt(1, 10)))) { //!place(board.board, Player2, sc.nextInt())
+                System.out.println("There's already a piece there. Please try again.");
+            }
+            board.print();
+            counter++;
+        }
+        endMessage(board);
+    }
+
+    public static void endMessage(Game board) {
+        if (board.end() == 'x') System.out.println("Player 1 wins!");
+        else if (board.end() == 'o') System.out.println("Player 2 wins!");
         else System.out.println("Draw.");
     }
 }
